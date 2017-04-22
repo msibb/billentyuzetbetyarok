1234a0c9e87c4e69a3af73a90cdf8bf129f0254d
Kovács Lászlo <kovacs.laszlo.varazslo@gmail.com>
2017-04-22 12:36:36 +0200

    robot class

---
+++ billentyuzetbetyarok/Robot.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace billentyuzetbetyarok
+{
+    public class Robot
+    {
+
+        public Robot(){
+            hajo_lerakas();
+        }
+
+        public void hajo_lerakas(){
+            Ships peldany = new Ships(); 
+        }
+        
+    }
+}
